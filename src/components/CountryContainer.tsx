2be702c2e1bbc77cd5b6eab46194f2bf445bae14
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-27 21:36:57 -0300

    feat: country container with actions

--- src/components/CountryContainer.tsx
+++ src/components/CountryContainer.tsx
@@ -1,28 +1,40 @@
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import styled from 'styled-components';
-import CountryCard from './CountryCard';
+import { useGetCountries } from '../hooks/useGetCountries';
+import CountryCards from './CountryCards';
+import RegionFilter from './RegionFilter';
+import SearchField from './SearchField';
 
 const MainContainer: any = styled(Container)`
   margin-top: 40px;
 `;
 
 export default function CountryContainer() {
+  const countries = useGetCountries();
+
+  console.log(countries);
+
   return (
-    <MainContainer maxWidth="lg">
-      <Grid
-        container
-        spacing={7}
-        direction="row"
-        justify="space-between"
-        alignItems="flex-start"
-      >
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-        <CountryCard />
-      </Grid>
-    </MainContainer>
+    <>
+      <MainContainer maxWidth="lg">
+        <Grid
+          container
+          direction="row"
+          justify="space-between"
+          alignItems="flex-start"
+        >
+          <Grid item xs={3}>
+            <SearchField />
+          </Grid>
+
+          <Grid item xs={2}>
+            <RegionFilter />
+          </Grid>
+        </Grid>
+      </MainContainer>
+
+      <CountryCards countries={countries} />
+    </>
   );
 }
