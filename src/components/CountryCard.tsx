b8dbf55c187e3be8e9cef7cac92ad723078f819a
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-27 21:35:48 -0300

    feat: receive information to the card

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -5,6 +5,8 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Grid from '@material-ui/core/Grid';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
+import { Link } from 'react-router-dom';
+import { addCommas } from '../utils';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,34 +15,40 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     media: {
       height: 140
+    },
+    cardLink: {
+      textDecoration: 'none'
     }
   })
 );
 
-export default function CardCountry() {
+export default function CardCountry({ country, to }: any = {}) {
   const classes = useStyles();
+  const { name, population, region, capital, flag, alpha3Code } = country;
+
+  const CardLink = (props: any) => <Link to={to} {...props} />;
 
   return (
     <Grid item xs={3}>
       <Card className={classes.cardStyle}>
-        <CardActionArea>
-          <CardMedia
-            className={classes.media}
-            image="https://restcountries.eu/data/irl.svg"
-            title="Ireland"
-          />
+        <CardActionArea
+          className={classes.cardLink}
+          component={CardLink}
+          to={`/country/${alpha3Code.toLowerCase()}`}
+        >
+          <CardMedia className={classes.media} image={flag} title={name} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
-              Ireland
+              {name}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              <b>Population:</b> 6,378,000
+              <b>Population:</b> {addCommas(population)}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              <b>Region:</b> Europe
+              <b>Region:</b> {region}
             </Typography>
             <Typography variant="body2" color="textSecondary" component="p">
-              <b>Capital:</b> Dublin
+              <b>Capital:</b> {capital}
             </Typography>
           </CardContent>
         </CardActionArea>
