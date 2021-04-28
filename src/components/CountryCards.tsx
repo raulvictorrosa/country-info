802a210904b1ae327c9cc909e0dd5efba3c0fbe3
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-27 21:36:26 -0300

    feat: component to the cards list

---
+++ src/components/CountryCards.tsx
@@ -0,0 +1,30 @@
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import styled from 'styled-components';
+import CountryCard from './CountryCard';
+
+const MainContainer: any = styled(Container)`
+  margin-top: 40px;
+`;
+
+export default function CountryCards({ countries }: any) {
+  return (
+    <MainContainer maxWidth="lg">
+      <Grid
+        container
+        spacing={7}
+        direction="row"
+        justify="space-between"
+        alignItems="flex-start"
+      >
+        {countries &&
+          countries.map((country: any) => (
+            <CountryCard
+              key={country.alpha3Code.toLowerCase()}
+              country={country}
+            />
+          ))}
+      </Grid>
+    </MainContainer>
+  );
+}
