a9687d3c3e990f24addf4e343262c3201ff545d7
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-28 08:37:26 -0300

    feat: api get country by region

--- src/api/countries.tsx
+++ src/api/countries.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const { REACT_APP_API_COUNTRIES } = process.env;
 
-const getAll = () => axios.get(`${REACT_APP_API_COUNTRIES}/all`);
+export const getAll = () => axios.get(`${REACT_APP_API_COUNTRIES}/all`);
 
-export default { getAll };
+export const getAllByRegion = (region: string) =>
+  axios.get(`${REACT_APP_API_COUNTRIES}/region${region}`);
