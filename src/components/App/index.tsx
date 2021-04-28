cd36c0e74551b2174aa2f7252f2606b4b0b629ef
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-28 15:01:27 -0300

    feat: initial state to countries

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,16 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme } from '@material-ui/core/styles';
+import { useReducer } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { CountriesProvider } from '../../providers/countriesProvider';
 import ThemeProvider from '../../providers/ThemeProvider';
+import { countriesReducer } from '../../reducers/countriesReducer';
 import CountryContainer from '../CountryContainer';
 import Header from '../Header';
+
+const initialState = {
+  countries: []
+};
 
 export default function App() {
   const theme = createMuiTheme({
@@ -12,14 +19,18 @@ export default function App() {
     }
   });
 
+  const state = useReducer(countriesReducer, initialState);
+
   return (
     <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Header />
+      <CountriesProvider value={state}>
+        <CssBaseline />
+        <Header />
 
-      <Switch>
-        <Route path="/" exact component={CountryContainer} />
-      </Switch>
+        <Switch>
+          <Route path="/" exact component={CountryContainer} />
+        </Switch>
+      </CountriesProvider>
     </ThemeProvider>
   );
 }
