101b16a08446890391cefec7ab6c137b98d88797
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-29 09:06:40 -0300

    feat: enhance theme provider and remove countries provider

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,36 +1,26 @@
-import CssBaseline from '@material-ui/core/CssBaseline';
 import { createMuiTheme } from '@material-ui/core/styles';
-import { useReducer } from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { CountriesProvider } from '../../providers/countriesProvider';
-import ThemeProvider from '../../providers/ThemeProvider';
-import { countriesReducer } from '../../reducers/countriesReducer';
+import Providers from '../../providers';
 import CountryContainer from '../CountryContainer';
+import CountrySingle from '../CountrySingle';
 import Header from '../Header';
 
-const initialState = {
-  countries: []
-};
-
-export default function App() {
+const App: React.FC = () => {
   const theme = createMuiTheme({
     palette: {
       type: 'dark'
     }
   });
 
-  const state = useReducer(countriesReducer, initialState);
-
   return (
-    <ThemeProvider theme={theme}>
-      <CountriesProvider value={state}>
-        <CssBaseline />
-        <Header />
-
-        <Switch>
-          <Route path="/" exact component={CountryContainer} />
-        </Switch>
-      </CountriesProvider>
-    </ThemeProvider>
+    <Providers theme={theme}>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={CountryContainer} />
+        <Route path="/country/:id" component={CountrySingle} />
+      </Switch>
+    </Providers>
   );
-}
+};
+
+export default App;
