c8d0344662aa38dae9859a4b2a242f58f9b86180
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-29 11:06:45 -0300

    feat: remove unnecessary call of material theme

--- src/components/SearchField.tsx
+++ src/components/SearchField.tsx
@@ -1,12 +1,12 @@
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
 import Paper from '@material-ui/core/Paper';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     inputRoot: {
       width: '100%'
