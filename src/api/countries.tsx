9dc18e3a850f831b51a957f0a5f43ed0e7023ad9
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-29 23:38:24 -0300

    feat: enhnace countries api

--- src/api/countries.tsx
+++ src/api/countries.tsx
@@ -2,10 +2,31 @@ import axios from 'axios';
 
 const { REACT_APP_API_COUNTRIES } = process.env;
 
-export const getAll = (text: string) =>
-  !text
-    ? axios.get(`${REACT_APP_API_COUNTRIES}/all`)
-    : axios.get(`${REACT_APP_API_COUNTRIES}/name/${text}`);
+export const initialFields: string[] = [
+  'alpha2Code',
+  'alpha3Code',
+  'borders',
+  'capital',
+  'currencies',
+  'flag',
+  'languages',
+  'name',
+  'nativeName',
+  'population',
+  'region',
+  'subregion',
+  'topLevelDomain',
+  'translations'
+];
 
-export const getAllByRegion = (region: string) =>
-  axios.get(`${REACT_APP_API_COUNTRIES}/region/${region}`);
+export const getAll = (text: string, fields = initialFields) =>
+  !text
+    ? axios.get(`${REACT_APP_API_COUNTRIES}/all?fields=${fields.join(';')}`)
+    : axios.get(
+        `${REACT_APP_API_COUNTRIES}/name/?fields=${text}${fields.join(';')}`
+      );
+
+export const getAllByRegion = (region: string, fields = initialFields) =>
+  axios.get(
+    `${REACT_APP_API_COUNTRIES}/region/?fields=${region}${fields.join(';')}`
+  );
--- src/api/country.tsx
+++ src/api/country.tsx
@@ -1,6 +1,10 @@
 import axios from 'axios';
+import { initialFields } from '../api/countries';
 
 const { REACT_APP_API_COUNTRIES } = process.env;
 
-export const getByAlpha = (alpha3: string) =>
-  alpha3 && axios.get(`${REACT_APP_API_COUNTRIES}/alpha/${alpha3}`);
+export const getByAlpha = (alpha3: string, fields = initialFields) =>
+  alpha3 &&
+  axios.get(
+    `${REACT_APP_API_COUNTRIES}/alpha/${alpha3}?fields=${fields.join(';')}`
+  );
