774510860a562f973c883dfa3f6590a8efb778dc
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-30 13:22:48 -0300

    feat: separete styled in new file and adjust responsiveness of component SearchField

--- src/components/SearchField/index.tsx
+++ src/components/SearchField/index.tsx
@@ -1,10 +1,10 @@
 import IconButton from '@material-ui/core/IconButton';
 import InputBase from '@material-ui/core/InputBase';
-import Paper from '@material-ui/core/Paper';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 import SearchIcon from '@material-ui/icons/Search';
 import { useEffect, useState } from 'react';
 import { useDebounce } from 'use-debounce';
+import { Paper } from '../SearchField/styled';
 
 const useStyles = makeStyles(() =>
   createStyles({
---
+++ src/components/SearchField/styled.tsx
@@ -0,0 +1,7 @@
+import { Paper as PaperStyled } from '@material-ui/core';
+import styled from 'styled-components';
+
+export const Paper = styled(PaperStyled)`
+  border: 1px solid;
+  border-color: rgba(255, 255, 255, 0.23);
+`;
