5a4db01f05ae3983b973355c0dbf2d1c9d41d920
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-29 23:41:17 -0300

    feat: remove unnecessary code

--- src/components/RegionFilter/index.tsx
+++ src/components/RegionFilter/index.tsx
@@ -1,29 +1,14 @@
 import Paper from '@material-ui/core/Paper';
-import { createStyles, makeStyles } from '@material-ui/core/styles';
 import { useEffect, useState } from 'react';
 import { TextFieldRegion } from '../RegionFilter/styled';
 
 const regions = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
-
-const useStyles = makeStyles(() =>
-  createStyles({
-    searchForm: {
-      paddingTop: '5px',
-      paddingBottom: '5px',
-      display: 'flex'
-    },
-    searchIcon: {
-      padding: 10
-    }
-  })
-);
 
 type FilterFieldProps = {
   onFilter: (text: string) => void;
 };
 
 const RegionFilter: React.FC<FilterFieldProps> = ({ onFilter }) => {
-  const classes = useStyles();
   const [filter, setFilter] = useState('');
 
   useEffect(() => onFilter(filter), [onFilter, filter]);
