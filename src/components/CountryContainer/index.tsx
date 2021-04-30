010a14b14e57db89aeae6f3dda4a3c341ca3bb87
Raul Rosa <raulvictorrosa@gmail.com>
2021-04-30 13:20:46 -0300

    feat: separete styled in new file and adjust responsiveness of component CountryContainer

--- src/components/CountryContainer/index.tsx
+++ src/components/CountryContainer/index.tsx
@@ -1,36 +1,31 @@
-import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import styled from 'styled-components';
 import { useGetCountries } from '../../hooks/useGetCountries';
 import CountryCards from '../CountryCards';
+import { Container } from '../CountryContainer/styled';
 import RegionFilter from '../RegionFilter';
 import SearchField from '../SearchField';
-
-const MainContainer: any = styled(Container)`
-  margin-top: 40px;
-`;
 
 export default function CountryContainer() {
   const { state, actions } = useGetCountries();
 
   return (
     <>
-      <MainContainer maxWidth="lg">
+      <Container maxWidth="lg">
         <Grid
           container
           direction="row"
           justify="space-between"
           alignItems="flex-start"
         >
-          <Grid item xs={6} sm={6} md={3}>
+          <Grid item xs={12} sm={6} md={3}>
             <SearchField onSearch={actions.setSearchText} />
           </Grid>
 
-          <Grid item xs={5} sm={5} md={3} lg={2}>
+          <Grid item xs={12} sm={5} md={3} lg={2}>
             <RegionFilter onFilter={actions.setFilterText} />
           </Grid>
         </Grid>
-      </MainContainer>
+      </Container>
 
       {!state.error && !state.isLoading && (
         <CountryCards countries={state.countries} />
